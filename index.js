7d70ef7062d48f763d773f1dcb92c3174e23a380
Aditya Patange <contact.adityapatange@gmail.com>
2024-03-02 15:06:59 +0530

    add bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { APP_NAME, APP_PORT } from "./commons/constants.js";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
